da090ed69adae2526f5970ac438c3f681634a2fb
Tomas Ye <tomaye@amazon.com>
2021-07-18 09:32:48 -0700

    [2D EXPERIMENTS] Sampling from object distribution works

--- data_providers/GeometricDataProvider2D.py
+++ data_providers/GeometricDataProvider2D.py
@@ -1,22 +1,28 @@
 import numpy as np
 import pandas as pd
 
-from expressions.Expression import Expression
-
 
 class GeometricDataProvider2D:
-    def __init__(self, equations: list, range_x1: tuple, range_x2: tuple, granularity: int = 1000):
-        self.granularity = granularity
-        self.x1, self.x2, self.y = self.build_df(equations, range_x1, range_x2)
+    def __init__(self, equations: list, range_x1: tuple, range_x2: tuple, num_of_points: int = 1000):
+        self.range_x2 = range_x2
+        self.range_x1 = range_x1
+        self.equations = equations
+        self.num_of_points = num_of_points
 
-    def get_data(self):
-        return self.x1, self.x2, self.y
+    def get_wrapper_df(self, x1, x2, y):
+        wrapper_df = pd.DataFrame()
+        wrapper_df['x_1'] = x1
+        wrapper_df['x_2'] = x2
+        wrapper_df['y'] = y
 
+        return wrapper_df
 
-    def evaluate_equations(self, equations, input_df, shape):
+    def evaluate_equations(self, x1, x2):
+        wrapper_df = self.get_wrapper_df(x1, x2, 0)
+
         results = []
-        for equation in equations:
-            results.append(equation.evaluate(input_df).to_numpy().reshape(shape))
+        for equation in self.equations:
+            results.append(equation.evaluate(wrapper_df))
 
         result = results[0]
         for i in range(1, len(results)):
@@ -24,16 +30,19 @@ class GeometricDataProvider2D:
 
         return result
 
+    def get_random_sample_from_distribution(self):
+        x1 = self.range_x1[0] + np.random.random(self.num_of_points) * (self.range_x1[1] - self.range_x1[0])
+        x2 = self.range_x2[0] + np.random.random(self.num_of_points) * (self.range_x2[1] - self.range_x1[0])
+        y = self.evaluate_equations(x1, x2)
 
+        return x1, x2, y
 
+    def get_distribution_contours(self):
+        step_x1 = (self.range_x1[1] - self.range_x1[0]) / self.num_of_points
+        step_x2 = (self.range_x2[1] - self.range_x2[0]) / self.num_of_points
 
-    def build_df(self, equations, range_x1, range_x2):
-        equation: Expression = equations[0]
-        step_x1 = (range_x1[1] - range_x1[0]) / self.granularity
-        step_x2 = (range_x2[1] - range_x2[0]) / self.granularity
-
-        x_1 = np.arange(range_x1[0], range_x1[1], step_x1)
-        x_2 = np.arange(range_x2[0], range_x2[1], step_x2)
+        x_1 = np.arange(self.range_x1[0], self.range_x1[1], step_x1)
+        x_2 = np.arange(self.range_x2[0], self.range_x2[1], step_x2)
         xx1, xx2 = np.meshgrid(x_1, x_2)
 
         input_df = pd.DataFrame()
@@ -41,10 +50,8 @@ class GeometricDataProvider2D:
         input_df['x_2'] = xx2.ravel()
 
         yy = self.evaluate_equations(
-            equations=equations,
-            input_df=input_df,
-            shape=xx1.shape
-        )
+            x1=xx1.ravel(),
+            x2=xx2.ravel()
+        ).to_numpy().reshape(xx1.shape)
 
         return xx1, xx2, yy
-
--- experiments/Simple2DExperiment.py
+++ experiments/Simple2DExperiment.py
@@ -1,13 +1,26 @@
 from matplotlib import pyplot as plt
 
+from data_providers.GeometricDataProvider2D import GeometricDataProvider2D
+
 
 class Simple2DExperiment:
     def __init__(self, model, data_provider):
-        self.data_provider = data_provider
+        self.data_provider: GeometricDataProvider2D = data_provider
         self.model = model
 
-    def run_experiment(self):
-        x1, x2, y = self.data_provider.get_data()
 
-        plt.contourf(x1, x2, y, alpha=0.4)
+    def plot_contours(self):
+        x1, x2, y = self.data_provider.get_distribution_contours()
+        plt.contourf(x1, x2, y, alpha=0.1)
+
+    def plot_samples(self):
+        x1, x2, y = self.data_provider.get_random_sample_from_distribution()
+        plt.scatter(x=x1, y=x2, c=y)
+
+    def run_experiment(self):
+        self.plot_contours()
+        self.plot_samples()
+
         plt.show()
+
+
