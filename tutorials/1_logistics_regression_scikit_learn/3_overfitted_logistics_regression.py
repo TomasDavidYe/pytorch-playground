04276980d30b185f135e580451ebc7ec20cde85d
Tomas Ye <tomaye@amazon.com>
2021-07-19 01:39:22 -0700

    [SCIKIT LEARN LOGISTICS REGRESSION] 1) Implemented simple linear logistics regression via Scikit Learn 2) Added polynomial features 3) Added Overfitting example
    TODO:
    1) Write a README section to the repository

---
+++ tutorials/1_logistics_regression_scikit_learn/3_overfitted_logistics_regression.py
@@ -0,0 +1,40 @@
+from data_providers.GeometricDataProvider2D import GeometricDataProvider2D
+from experiments.Simple2DExperiment import Simple2DExperiment
+from expressions.expression_shortcuts import x_1, x_2, const
+from models.LogisticsClassifier2DScikitLearn import LogisticsClassifier2DScikitLearn
+
+
+def feature_transform(x1, x2):
+    return [
+        x1,
+        x1 ** 2,
+        x1 ** 3,
+        x1 ** 4,
+        x1 ** 5,
+        x2,
+        x2 ** 2,
+        x2 ** 3,
+        x2 ** 4,
+        x2 ** 5,
+    ]
+
+
+provider = GeometricDataProvider2D(
+    equations=[
+        x_1() ** 2 - x_2() <= const(1),
+        x_2() <= const(1)
+    ],
+    range_x1=(-2, 2),
+    range_x2=(-2, 2),
+    num_of_samples=50,
+    plot_granularity=200
+)
+
+model = LogisticsClassifier2DScikitLearn(
+    feature_transform=feature_transform
+)
+
+Simple2DExperiment(
+    model=model,
+    data_provider=provider
+).run_experiment()
